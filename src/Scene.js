eee3bb833d97690678fbd3b5a29f49e87d98041b
Aura Munoz <aura.munoz@autodesk.com>
2019-06-15 17:13:07 +0200

    add timeline and trajectories

--- src/Scene.js
+++ src/Scene.js
@@ -1,29 +1,55 @@
 import * as THREE from 'three';
+import OrbitControlsAux from 'three-orbit-controls';
+const OrbitControls = OrbitControlsAux(THREE);
 
-export const createScene = (dimensions, canvas)=>{
+export const createScene = (
+  dimensions={height: window.innerHeight, width: window.innerWidth},
+  planets = [],
+  trajectories = [],
+  )=>{
   let camera, scene, renderer;
-  let mesh;
   init();
   animate();
   function init() {
     scene = new THREE.Scene();
-camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-renderer = new THREE.WebGLRenderer(canvas);
+    renderer = new THREE.WebGLRenderer();
 
-renderer.setSize( window.innerWidth, window.innerHeight );
-  var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-mesh = new THREE.Mesh( geometry, material );
-  scene.add( mesh );
-  camera.position.z = 5;
+    const planetFactor = 60268;
+    // Object.keys(planets).forEach((planetName)=> {
+    //   const planet = planets[planetName];
+    //   var texture = new THREE.TextureLoader().load( planet.texture );
+    //   var geometry = new THREE.SphereBufferGeometry( planet.radius / planetFactor, 32, 32 );
+    //   var material = new THREE.MeshBasicMaterial( { map: texture } );
+    //   const planetMesh = new THREE.Mesh( geometry, material );
+    //   scene.add( planetMesh );
+    // })
+
+    const factor = 10;
+    trajectories.forEach((bodyTrajectories)=>{
+      const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+      const geometry = new THREE.Geometry();
+      bodyTrajectories.forEach((trajectory)=>{
+        geometry.vertices.push(new THREE.Vector3( factor*trajectory.x, factor*trajectory.y, factor*trajectory.z) );
+      });
+      
+      const line = new THREE.Line( geometry, material );
+      scene.add(line);
+    });
+
+    // Setting up controllers 
+    const controls = new OrbitControls( camera, renderer.domElement );
+
+
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    camera.position.z = 5;
   }
 
 
   function animate() {
     requestAnimationFrame( animate );
-    mesh.rotation.x += 0.005;
-    mesh.rotation.y += 0.01;
     renderer.render( scene, camera );
   }
   return renderer;
